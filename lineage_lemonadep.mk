f160530962fa5e6862eac1dbd25fb39dafc3c0f1
nshorty <andrysciortino@gmail.com>
2024-05-27 23:05:01 +0200

    RisingOS!

--- lineage_lemonadep.mk
+++ lineage_lemonadep.mk
@@ -11,8 +11,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lemonadep device
 $(call inherit-product, device/oneplus/lemonadep/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common Bliss stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# RisingOS Flags
+WITH_GMS := true
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RISING_CHIPSET="Snapdragon 888 5G" \
+    RISING_MAINTAINER="nshorty"
 
 PRODUCT_NAME := lineage_lemonadep
 PRODUCT_DEVICE := lemonadep
@@ -26,8 +39,8 @@ PRODUCT_SYSTEM_DEVICE := OnePlus9Pro
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus9Pro-user 13 TP1A.220905.001 R.159cc68-4eae-182baf release-keys" \
+    PRIVATE_BUILD_DESC="OnePlus9Pro-user 14 UKQ1.230924.001 R.1695d68-52ce-52cd release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := OnePlus/OnePlus9Pro/OnePlus9Pro:13/TP1A.220905.001/R.159cc68-4eae-182baf:user/release-keys
+BUILD_FINGERPRINT := OnePlus/OnePlus9Pro/OnePlus9Pro:14/UKQ1.230924.001/R.1695d68-52ce-52cd:user/release-keys
